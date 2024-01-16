71a9947c5575927dd5b7d147e34db63e48e36e9e
Amal Jose <amaljose197@gmail.com>
2024-01-16 07:36:46 +0530

    update route

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/contact', router)
+app.use('/contacts', router)
 
 app.listen(3000, function () {
     console.log('Server listening on port 3000');
